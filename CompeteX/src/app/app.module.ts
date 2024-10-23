a3b266836977b49559dd938cd7ad2cdcec1025a0
Lucas Pedersen <Lucas-l-h-p@hotmail.dk>
2024-10-23 13:32:55 +0200

    Initial navBar is created

--- CompeteX/src/app/app.module.ts
+++ CompeteX/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,6 +17,9 @@ import { CalenderComponent } from './calender/calender.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
     AppRoutingModule
   ],
   providers: [
