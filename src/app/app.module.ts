72107573aa41eea5b7683dd863bcc37f56b918a4
Lucas Pedersen <Lucas-l-h-p@hotmail.dk>
2024-10-25 13:50:41 +0200

    Fixed navbar for mobile use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { AppRoutingModule } from './app-routing.module';
@@ -30,7 +31,8 @@ import { NavbarComponent } from './basic-components/navbar/navbar.component';
     MatListModule,
     AppRoutingModule, 
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [
     provideAnimationsAsync()
