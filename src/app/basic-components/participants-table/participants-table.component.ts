69b9b2eb9002fa82d4ea0e6eb290ac7d14478c69
Pætur <pmtv96@gmail.com>
2024-10-30 11:44:45 +0100

    imlemented the mocd data for the participant

--- src/app/basic-components/participants-table/participants-table.component.ts
+++ src/app/basic-components/participants-table/participants-table.component.ts
@@ -1,6 +1,8 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, ViewChild} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Participant } from '../../models/participant';
+import { PARTICIPANT_DATA } from '../../mock-data/mock-participant'
 
 @Component({
   selector: 'app-participants-table',
@@ -20,33 +22,4 @@ export class ParticipantsTableComponent {
 }
 
 // Shold be dynamic depending on the requirments
-export interface Participant {
-  number: number;
-  name: string;
-  bunny: string;
-  startno: number;
-}
 
-const PARTICIPANT_DATA: Participant[] = [
-  { number: 1, name: 'Alice', bunny: 'Asteroid Destroyer', startno: 101 },
-  { number: 2, name: 'Bob', bunny: 'Optimus Prime', startno: 102 },
-  { number: 3, name: 'Charlie', bunny: 'Galactic Conqueror', startno: 103 },
-  { number: 4, name: 'Daisy', bunny: 'Thunder Bunny', startno: 104 },
-  { number: 5, name: 'Edward', bunny: 'Star Hopper', startno: 105 },
-  { number: 6, name: 'Fiona', bunny: 'Comet Chaser', startno: 106 },
-  { number: 7, name: 'George', bunny: 'Supernova Slayer', startno: 107 },
-  { number: 8, name: 'Hannah', bunny: 'Nebula Navigator', startno: 108 },
-  { number: 9, name: 'Ian', bunny: 'Rocket Racer', startno: 109 },
-  { number: 10, name: 'Jasmine', bunny: 'Meteor Masher', startno: 110 },
-  { number: 11, name: 'Kevin', bunny: 'Cosmos Crusher', startno: 111 },
-  { number: 12, name: 'Lily', bunny: 'Galaxy Gladiator', startno: 112 },
-  { number: 13, name: 'Mike', bunny: 'Planet Pulverizer', startno: 113 },
-  { number: 14, name: 'Nina', bunny: 'Orbit Overlord', startno: 114 },
-  { number: 15, name: 'Oscar', bunny: 'Starlight Samurai', startno: 115 },
-  { number: 16, name: 'Paula', bunny: 'Lunar Legend', startno: 116 },
-  { number: 17, name: 'Quinn', bunny: 'Astro Avenger', startno: 117 },
-  { number: 18, name: 'Rachel', bunny: 'Meteor Monarch', startno: 118 },
-  { number: 19, name: 'Steve', bunny: 'Solar Smasher', startno: 119 },
-  { number: 20, name: 'Tina', bunny: 'Black Hole Bandit', startno: 120 },
-  { number: 21, name: 'Peter', bunny: 'Bour, God of Stumper', startno: 121 }
-];
