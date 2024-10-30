dd58678d0606c38f773eaf478a20189496bfe60b
Pætur <pmtv96@gmail.com>
2024-10-30 11:15:09 +0100

    created the table, and it is possible to expand the table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CalenderComponent } from './calender/calender.component';
+import { CompetitionComponent } from './competition/competition.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'calender', component: CalenderComponent},
-  { path: '', redirectTo: 'home', pathMatch: 'full'}
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'competition', component: CompetitionComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDivider } from '@angular/material/divider';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { InMemoryDataService } from './service/InMomoryData/in-memory-data.service';
 
 import { ButtonComponent } from './basic-components/button/button.component';
 import { HomeComponent } from './home/home.component';
 import { CalenderComponent } from './calender/calender.component';
 import { EventTableComponent } from './basic-components/event-table/event-table.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CompetitionComponent } from './competition/competition.component';
 import { ParticipantsTableComponent } from './basic-components/participants-table/participants-table.component';
 
@@ -49,7 +51,9 @@ import { ParticipantsTableComponent } from './basic-components/participants-tabl
     MatExpansionModule,
     MatProgressSpinner,
     MatDivider,
-    HttpClientModule,
+    MatTableModule, 
+    MatPaginatorModule,
+      HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false}
     )
