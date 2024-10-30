04faf6a17c71da1df29a9e9f7d8e388f02faa865
Pætur <pmtv96@gmail.com>
2024-10-30 14:24:06 +0100

    checkpoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Adjust import as necessary
 import { CalenderComponent } from './calender/calender.component'; // Adjust import as necessary
 import { MyPageComponent } from './mypage/mypage.component'; // Adjust import as necessary
-import { HomeComponent } from './home/home.component';
-import { CalenderComponent } from './calender/calender.component';
 import { CompetitionComponent } from './competition/competition.component';
 
 const routes: Routes = [
@@ -12,7 +10,7 @@ const routes: Routes = [
   { path: 'calendar', component: CalenderComponent },
   { path: 'mypage', component: MyPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to home
-  { path: '**', redirectTo: '/home' } // Redirect unknown paths to home
+  { path: '**', redirectTo: '/home' }, // Redirect unknown paths to home
   { path: 'home', component: HomeComponent},
   { path: 'calender', component: CalenderComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full'},
