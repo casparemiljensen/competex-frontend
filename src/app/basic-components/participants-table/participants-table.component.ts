c5e3a94dab47f68148dd4a1302c8e1fa3cc6637a
Pætur <pmtv96@gmail.com>
2024-11-01 10:38:17 +0100

    made the partipicipant table dynamic in collum creation

--- src/app/basic-components/participants-table/participants-table.component.ts
+++ src/app/basic-components/participants-table/participants-table.component.ts
@@ -1,8 +1,6 @@
-import { AfterViewInit, Component, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { Participant } from '../../models/participant';
-import { PARTICIPANT_DATA } from '../../mock-data/mock-participant'
 
 @Component({
   selector: 'app-participants-table',
@@ -10,12 +8,21 @@ import { PARTICIPANT_DATA } from '../../mock-data/mock-participant'
   styleUrl: './participants-table.component.css',
 })
   
-export class ParticipantsTableComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<Participant>(PARTICIPANT_DATA);
-
+export class ParticipantsTableComponent implements OnInit {
+  @Input() data: any[] = [];
+  @Input() columns: string[] = [];
+  
+  // columnsToDisplay: string[] = this.displayedColumns.slice();
+  public dataSource = new MatTableDataSource<any>();
+  public displayedColumns: string[] = [];
+  
+  ngOnInit(): void {
+    this.dataSource.data = this.data;
+    this.displayedColumns = this.columns;
+  }
+  
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-
+  
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
--- src/app/competition/competition.component.ts
+++ src/app/competition/competition.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PARTICIPANT_DATA } from '../mock-data/mock-participant';
 
 @Component({
   selector: 'app-competition',
   templateUrl: './competition.component.html',
   styleUrl: './competition.component.css'
 })
-export class CompetitionComponent {
+export class CompetitionComponent implements OnInit{
+  
+  public tables: any[] = [];
+
+  ngOnInit(): void {
+    
+    // this.tables = PARTICIPANT_DATA
+    this.tables = [
+      {
+        data: [
+          { id: 1, name: 'Alice', age: 24 },
+          { id: 2, name: 'Bob', age: 30 }
+        ],
+        columns: ['id', 'name', 'age']
+      },
+      {
+        data: [
+          { product: 'Table', price: 300 },
+          { product: 'Chair', price: 150 }
+        ],
+        columns: ['product', 'price']
+      }
+    ];
+  }
 
 }
