01acc1fe3ea232e32ae4ce2fb222bbef706de29f
Ellleby <charlotteelleby@gmail.com>
2024-11-13 12:45:42 +0100

    Fixed error, navbar + calender routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,24 +4,22 @@ import { HomeComponent } from './home/home.component';
 import { CalenderComponent } from './calender/calender.component';
 import { CreateEventComponent } from './create-event/create-event.component';
 import { MyPageComponent } from './mypage/mypage.component'; // Adjust import as necessary
-import { CompetitionComponent } from './basic-components/competition/competition.component';
 import { CompStartComponent } from './comp-start/comp-start.component';
 import { EventPageComponent } from './event-page/event-page.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent},
-  { path: 'calender', component: CalenderComponent},
-  { path: 'create-event', component: CreateEventComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'calender', component: CalenderComponent },
+  { path: 'create-event', component: CreateEventComponent },
   { path: 'mypage', component: MyPageComponent },
-  { path: 'competition', component: CompetitionComponent},
-  { path: 'competition-start', component: CompStartComponent},
-  { path: 'event-page', component: EventPageComponent},
+  { path: 'competition-start', component: CompStartComponent },
+  { path: 'event-page', component: EventPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to home
-  { path: '**', redirectTo: '/home' } // Redirect unknown paths to home
+  { path: '**', redirectTo: '/home' }, // Redirect unknown paths to home
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
