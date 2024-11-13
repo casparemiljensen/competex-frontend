a1eba0c9eb823b3d218d6d71366426722a2a4bc9
Pætur <pmtv96@gmail.com>
2024-11-13 13:37:09 +0100

    updated name from compititon service to expandebletable service

--- src/app/service/ExpandableTable/expandabletable.service.spec.ts
+++ src/app/service/ExpandableTable/expandabletable.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ExpandableTableService } from './ExpandableTable.service';
+import { ExpandableTableService } from './expandabletable.service';
 
 describe('ExpandableTableService', () => {
   let service: ExpandableTableService;
