22fa0e5f19e6d7ef587d50a9bf4a9c70af251d95
Pætur <pmtv96@gmail.com>
2024-11-15 14:55:43 +0100

    the results overview page interaction works as it should do

--- src/app/results-overview-page/results-overview-page.component.ts
+++ src/app/results-overview-page/results-overview-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EventService } from '../service/event/event.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-results-overview-page',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class ResultsOverviewPageComponent {
   title = "Resultater for tidliger stævner";
+
+  events: any[] = [];
+
+  constructor(private eventService: EventService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.eventService.getEvents().subscribe((data) => {
+      this.events = data;
+    });
+  }
+
+  viewEventResults(eventId: string) {
+    this.router.navigate([`${eventId}/results`]);
+  }
 }
