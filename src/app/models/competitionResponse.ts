8634aa580aec7f65feda3e23790bb2a0f124e800
Pætur <pmtv96@gmail.com>
2024-11-27 14:30:04 +0100

    updated spelling error

--- src/app/models/competitionResponse.ts
+++ src/app/models/competitionResponse.ts
@@ -1,8 +1,8 @@
-import { CompetitionType } from './competitionType';
+import { competitionType } from './competitionType';
 import { Level, Status } from './enums';
 export interface CompetitionResponse {
   id: string; // UUID
-  competitionType: CompetitionType; // Replace [...] with the expected type, e.g., string[] or an enum
+  competitionType: competitionType[]; // Replace [...] with the expected type, e.g., string[] or an enum
   startDate: string; // ISO date string
   endDate: string; // ISO date string
   level: Level; // Enum for Level
