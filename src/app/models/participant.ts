cfa51443fa7deeb46039ddfc8827ff28e160f52e
Ellleby <charlotteelleby@gmail.com>
2024-11-27 15:38:30 +0100

    models needed for match

--- src/app/models/participant.ts
+++ src/app/models/participant.ts
@@ -1,6 +1,27 @@
+import { Entity } from './entity';
+import { Member } from './member';
+
 export interface Participant {
-    number: number;
-    name: string;
-    bunny: string;
-    startno: number;
-  }
+  id: string;
+  name: string;
+  $type: string;
+}
+
+export interface Team extends Participant {
+  members?: Member[];
+  memberIds?: string[];
+}
+
+export interface Single extends Participant {
+  member?: Member;
+  memberId?: string;
+}
+
+export interface Ekvipage extends Participant {
+  member?: Member;
+  memberId?: string;
+  entity?: Entity;
+  entityId?: string;
+}
+
+export type ParticipantType = Team | Single | Ekvipage;
