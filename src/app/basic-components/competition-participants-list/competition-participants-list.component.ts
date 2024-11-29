fea35595cb8c50f553570969bdb47c65816c1095
Ellleby <charlotteelleby@gmail.com>
2024-11-29 10:25:50 +0100

    startlist match fetch.

--- src/app/basic-components/competition-participants-list/competition-participants-list.component.ts
+++ src/app/basic-components/competition-participants-list/competition-participants-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { Match } from '../../models/match';
+import { MatchService } from '../../service/match/match.service';
 
 @Component({
   selector: 'app-competition-participants-list',
@@ -7,12 +9,20 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrl: './competition-participants-list.component.css',
 })
 export class CompetitionParticipantsListComponent {
+  @Input() roundID: any = null;
   @Input() participants: { name: string }[] = [];
   @Output() selectedParticipantChange = new EventEmitter<number>();
+
   displayedColumns: string[] = ['number', 'name'];
   dataSource = new MatTableDataSource(this.participants);
   selectedParticipant: any = null;
+  matches: Match[] = [];
 
+  constructor(private matchService: MatchService) {}
+
+  ngOnInit(): void {
+    this.fetchMatches();
+  }
   ngOnChanges(): void {
     this.dataSource.data = this.participants;
   }
@@ -21,5 +31,17 @@ export class CompetitionParticipantsListComponent {
     this.selectedParticipant = participant;
     const index = this.participants.indexOf(participant);
     this.selectedParticipantChange.emit(index);
+  }
+
+  fetchMatches(): void {
+    this.matchService.getMatches().subscribe({
+      next: (data) => {
+        this.matches = data;
+        console.log('Matches:', this.matches);
+      },
+      error: (err) => {
+        console.log('Error:', err);
+      },
+    });
   }
 }
