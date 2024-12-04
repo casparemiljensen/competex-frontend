c54f286ae7a6ae50b7523c0a60c7fab4d6b2a899
Pætur <pmtv96@gmail.com>
2024-12-04 15:31:38 +0100

    updated comp-start page to work with backend

--- src/app/comp-start/comp-start.component.ts
+++ src/app/comp-start/comp-start.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../basic-components/confirm-dialog/confirm-dialog.component';
-import { EventPageService } from '../service/event-page/event-page.service';
+import { eventRespons } from '../models/eventRespons';
+import { ActivatedRoute } from '@angular/router';
+import { EventService } from '../service/eventTest/event-test.service';
+import { Status } from '../models/enums'
 
 @Component({
   selector: 'app-comp-start',
@@ -10,27 +12,60 @@ import { EventPageService } from '../service/event-page/event-page.service';
   styleUrls: ['./comp-start.component.css']
 })
 export class CompStartComponent implements OnInit {
-  ExpandableTableData: any[] = [];
-  judgeData: any[] = [];
-  displayedColumns: string[] = [];
+  event!: eventRespons;
   isLoading = true;
-
+  test!: any[]
+  public status = Status
+  
   @Output() buttonClick = new EventEmitter<void>();
 
-  constructor(private http: HttpClient, public dialog: MatDialog, private eventPageService: EventPageService) {}
+  constructor(
+    public dialog: MatDialog, 
+    private route: ActivatedRoute,
+    private EventService: EventService
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const eventId = params.get('eventId');
+      if (eventId) {
+        this.fetchEvent(eventId);
+      } else {
+        // handle empty repons here
+        console.error('Event ID is missing in the route.');
+      }
+    });
+
     this.isLoading = false;
+  }
 
-    // Fetch judge data
-    this.eventPageService.getJudge().subscribe(data => {
-      this.judgeData = data;
+  fetchEvent(eventId: string): void{
+    this.EventService.getEventById(eventId).subscribe({
+      next: (response) => {
+        this.event = response;
+        console.log(this.event)
+      },
+      error: (err) => console.error('Error fetching competitions:', err),
     });
+  }
 
-    // Fetch ExpandableTable data
-    this.eventPageService.getExpandableTables().subscribe(data => {
-      this.ExpandableTableData = data;
-    });
+  getStatusString(status: number): string {
+    if(status === 0)
+    {
+      return "Start"
+    }
+    if (status === 1) 
+    {
+      return "Aktiv"
+    }
+    if( status === 2 || status === 3)
+    {
+      return "Afslutet"
+    }
+    else
+    {
+      return Status[status]
+    }
   }
 
   openConfirmDialog(buttonText: string, comp: any): void {
