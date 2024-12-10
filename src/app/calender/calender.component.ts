26cc9f30ee8dd73b3963f2958c8a00f6b3149c6e
Pætur <pmtv96@gmail.com>
2024-12-10 15:36:59 +0100

    cleand up calendar, and event page

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,40 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
-import { EventService } from '../service/event/event.service';
-import { eventRespons } from '../models/eventRespons';
 
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
-  styleUrls: ['./calender.component.css'], // Fix typo: `styleUrl` -> `styleUrls`
+  styleUrls: ['./calender.component.css'], 
 })
 export class CalenderComponent implements OnInit {
   title = 'Stævne kalender';
-  events$: Observable<eventRespons[]> | undefined; // Observable to hold event data
 
   // Inject the EventService
   constructor(
-    private eventService: EventService,
-    private route: ActivatedRoute
   ) {}
 
-  // Use Angular's lifecycle hook to fetch events on component initialization
   ngOnInit(): void {
-    this.route.paramMap.subscribe((params) => {
-      const eventId = params.get('id');
-      if (eventId) {
-        console.log('test');
-      } else {
-        // handle empty repons here
-        console.error('Event ID is missing in the route.');
-      }
-      // this.fetchEvents();
-    });
-  }
 
-  // Fetch events from the service
-  fetchEvents(): void {
-    this.events$ = this.eventService.getEvents();
-  }
+  };
 }
