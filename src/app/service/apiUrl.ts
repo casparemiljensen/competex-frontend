9a98b3556fee10b7fe4b6e95953fbed0eb4ea93e
Ellleby <charlotteelleby@gmail.com>
2024-12-14 16:20:20 +0100

    Fixed Event Endpoint

--- src/app/service/apiUrl.ts
+++ src/app/service/apiUrl.ts
@@ -1,3 +1,2 @@
-// export const API_DOMAIN = 'https://competex-backend.schnykjaer.com:22114/'
-export const API_DOMAIN = 'https://competex-backend.schnykjaer.com:22114/api';
-
+export const API_DOMAIN = 'http://localhost:5228/api';
+// export const API_DOMAIN = 'https://competex-backend.schnykjaer.com:22114/api';
