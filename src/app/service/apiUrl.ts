611ef4fcd6c49cd73470e9f15ea8a96bf28ce114
casparejensen <casparejensen@gmail.com>
2024-12-20 00:49:42 +0100

    changed api url

--- src/app/service/apiUrl.ts
+++ src/app/service/apiUrl.ts
@@ -1,2 +1,2 @@
-export const API_DOMAIN = 'http://localhost:5228/api';
+export const API_DOMAIN = 'https://localhost:7140/api';
 // export const API_DOMAIN = 'https://competex-backend.schnykjaer.com:22114/api';
