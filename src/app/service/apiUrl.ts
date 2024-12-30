012238a5abab240ab8a2dcc10dc5e3bcd6faa383
Thomas Ilum Andersen <Thomas.ilum.andersen@gmail.com>
2024-12-30 15:05:55 +0100

    Changed backend address to match internal ClusterIP DNS in apiUrl.ts

--- src/app/service/apiUrl.ts
+++ src/app/service/apiUrl.ts
@@ -1,2 +1,4 @@
-export const API_DOMAIN = 'https://localhost:7140/api';
+// export const API_DOMAIN = 'https://localhost:7140/api';
 // export const API_DOMAIN = 'https://competex-backend.schnykjaer.com:22114/api';
+export const API_DOMAIN = 'http://competex-backend-service.competex-backend.svc.cluster.local:8080/api';
+// http://competex-backend-service.competex-backend.svc.cluster.local:8080/**
