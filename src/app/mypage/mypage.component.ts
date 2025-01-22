3a2a6295f3bb6e3552c57f32924a5d174e545e6a
Ellleby <charlotteelleby@gmail.com>
2025-01-22 21:47:01 +0100

    Small fixes of everything...
    Small fixes of everything...

--- src/app/mypage/mypage.component.ts
+++ src/app/mypage/mypage.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calender',
   templateUrl: './mypage.component.html',
-  styleUrl: './mypage.component.css'
+  styleUrl: './mypage.component.css',
 })
 export class MyPageComponent {
-  title = "this is the tile of the mypage"
+  title = 'Denne side er endnu under udvikling';
 }
