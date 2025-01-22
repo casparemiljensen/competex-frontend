1609b9306e8880335e674feb387be44da8569539
Thomas Ilum Andersen <Thomas.ilum.andersen@gmail.com>
2025-01-22 22:40:45 +0100

    Changed backend url to ingress endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://competex-backend-service.competex-backend.svc.cluster.local:8080/api'
+    apiUrl: 'http://backend.localhost/api'
 };
